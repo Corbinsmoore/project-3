584e8ad95a1f9955b08ee6263304180aae59a7a4
jdolinsky <jacobdol@jacobs-air.lan>
2024-01-03 19:21:02 -0600

    added cors policy for flask. added initial js logic

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask,jsonify
 import data_logic
 
 app = Flask(__name__)
 
 @app.route("/all")
 def all_data():
-    return data_logic.get_all_data()
+    response = jsonify(data_logic.get_all_data())
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 @app.route("/cjj")
 def cjj_data():
-    return data_logic.get_cjj_data()
+    response = jsonify(data_logic.get_cjj_data())
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
