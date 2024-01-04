e42d43364f8392836829e7a0378627d3dfe04da2
jdolinsky <jacobdol@jacobs-air.lan>
2024-01-04 12:43:14 -0600

    split data on to different names. sum all years from each state per name

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,8 +6,19 @@
 url_cjj = "http://127.0.0.1:5000/cjj"
 // Fetch the JSON data and console log it
 d3.json(url_cjj).then(function(data) {
-  console.log(data);
+  console.log("loaded data");
+  let joseph = {}, jacob = {}, corbin = {}
+  for (const item of data) {
+    if (item.name == "Corbin"){
+        (item.year in corbin)? corbin[item.year] += item.number : corbin[item.year] = item.number
+    } else if (item.name == "Joseph") {
+        (item.year in joseph)? joseph[item.year] += item.number : joseph[item.year] = item.number
+    } else {
+        (item.year in jacob)? jacob[item.year] += item.number : jacob[item.year] = item.number
+    }
+  }
+
+  console.log("jacob", jacob)
 });
-//filter Joseph
 
 // ***** END Jacob's part *************************
